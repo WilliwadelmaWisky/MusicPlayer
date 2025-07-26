850000957cf7a0f5809f73bd7a4692ada4168bac
WilliwadelmaWisky <minecraftprofile52@gmail.com>
2025-07-26 15:29:06 +0300

    Added a jar artifact.

---
+++ src/main/java/com/williwadelmawisky/musicplayer/RunMain.java
@@ -0,0 +1,14 @@
+package com.williwadelmawisky.musicplayer;
+
+/**
+ * A Wrapper class for getting the built artifact jar to run
+ */
+public class RunMain {
+
+    /**
+     * @param args
+     */
+    public static void main(final String[] args) {
+        Main.main(args);
+    }
+}
